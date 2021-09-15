553ffea152a15346311597638fd52e4c1b968514
DrusMarkonii <andrey8566ok@gmail.com>
2021-09-16 00:01:08 +0300

    add classes component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import './App.css';
-import './scss/app.scss';
+import { Component } from "react";
+import "./App.css";
+import "./scss/app.scss";
+import ToDoItem from "./ToDoItem/ToDoItem";
+import todosData from "./ToDoItem/todosData";
 
-function App() {
-  return (
-    <div className="App">
-      Ready to work
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todoItem : todosData
+    }
+  }
+
+  handleChange = (id) => {
+    console.log("Значение поменялось", id);
+  };
+
+  render() {
+    const {todoItem} = this.state;
+    const activeTasks = todoItem.map(item => {
+      return (
+        <ToDoItem
+          key={item.id}
+          description={item.description}
+          completed={item.complited}
+          handleChange={() => {
+            this.handleChange(item.id);
+          }}
+        />
+      );
+    })
+  
+    return (
+      <div className="App">
+        {activeTasks}
+      </div>
+    )
+      
+  }
 }
 
 export default App;
---
+++ src/ToDoItem/todosData.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const todosData = [
+    {
+        id: 0,
+        description: 'Сходить в магазин',
+        complited: false
+    },
+    {
+        id: 1,
+        description: 'Сварить борщ',
+        complited: false
+    },
+    {
+        id: 2,
+        description: 'Выкинуть мусор',
+        complited: false
+    },
+    {
+        id: 3,
+        description: 'Сделать задание',
+        complited: false
+    },
+    {
+        id: 4,
+        description: 'Написать программу',
+        complited: false
+    }
+]
+
+export default todosData
